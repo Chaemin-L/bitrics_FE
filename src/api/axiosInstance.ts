1f6f3f624c2389f9a4564397efc049188d084515
Chaemin-L <chaem12@kookmin.ac.kr>
2024-08-29 11:34:15 +0900

    chore: axios 설정

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,55 @@
+import axios, {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const isDev = process.env.NODE_ENV === "development";
+
+const devLog = (log: string) => {
+  isDev ? console.log(log) : null;
+};
+
+const onRequest = (config: InternalAxiosRequestConfig) => {
+  const { url, method } = config;
+  devLog(`[API REQUEST] ${method?.toUpperCase()} ${url}`);
+
+  return config;
+};
+
+const onRequestError = (config: AxiosRequestConfig) => {
+  const { url, method } = config;
+  devLog(`[API REQUEST ERROR] ${method?.toUpperCase()} ${url}`);
+
+  return Promise.reject(config);
+};
+
+const onResponse = (response: AxiosResponse) => {
+  const { status } = response;
+  const { url, method } = response.config;
+  devLog(`[API RESPONSE ${status}] ${method?.toUpperCase()} ${url}`);
+
+  return response;
+};
+
+const onResponseError = (error: AxiosError | Error) => {
+  if (axios.isAxiosError(error)) {
+    const { url, method } = error.request;
+    devLog(`[API REQUEST ERROR ${method?.toUpperCase()}] ${url}`);
+  }
+
+  return Promise.reject(error);
+};
+
+axios.interceptors.request.use(onRequest, onRequestError);
+axios.interceptors.response.use(onResponse, onResponseError);
+
+export default axiosInstance;
