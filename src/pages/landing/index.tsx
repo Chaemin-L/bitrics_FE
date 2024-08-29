0c43a542abae6ec9032575bea181497eddc8cda7
Chaemin-L <chaem12@kookmin.ac.kr>
2024-08-29 17:56:26 +0900

    chore: 폴더 대소문자 변경

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,12 @@
+import Test from "@/components/landing/test";
+
+const LandingPage = () => {
+  return (
+    <div className="text-darkPurple-400 text-[24px]">
+      This is LandingPage
+      <Test />
+    </div>
+  );
+};
+
+export default LandingPage;
