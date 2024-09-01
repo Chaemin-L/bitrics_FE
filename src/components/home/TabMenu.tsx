7aa645e4d77ac43d17a4a1632f5613f3ec65d114
seyeon326 <lseyeon0723@gmail.com>
2024-09-01 18:11:35 +0900

    add: 홈 김프 탭에 필터링 헤더 추가

--- src/components/home/TabMenu.tsx
+++ src/components/home/TabMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import clsx from "clsx";
+import FilteringHeader from "../news/FilteringHeader";
 
 interface TabMenuProps {
   tabs: string[];
@@ -8,6 +9,12 @@ interface TabMenuProps {
 
 const TabMenu: React.FC<TabMenuProps> = ({ tabs, children }) => {
   const [activeTab, setActiveTab] = useState(0);
+  const [selectedFilter, setSelectedFilter] = useState("KRW");
+  const keywords = [
+    { key: "KRW", label: "KRW" },
+    { key: "BTC", label: "BTC" },
+    { key: "USDT", label: "USDT" },
+  ];
 
   return (
     <div className="max-w-xl mx-auto mt-4">
@@ -27,6 +34,13 @@ const TabMenu: React.FC<TabMenuProps> = ({ tabs, children }) => {
               {tab}
             </div>
           ))}
+        </div>
+        <div className="mt-4">
+          <FilteringHeader
+            keywords={keywords}
+            selected={selectedFilter}
+            setSelected={setSelectedFilter}
+          />
         </div>
         <div className="text-white mt-4 -mx-[30px]">{children[activeTab]}</div>
       </div>
