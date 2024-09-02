6732d944a5a754d6544b43e7c5af6f639634f70d
seyeon326 <lseyeon0723@gmail.com>
2024-09-02 23:58:53 +0900

    fix: 스크롤 버튼이 열려있을 때 다른 버튼을 클릭해도 차트가 바뀌지 않는 오류 해결

--- src/components/home/TradingViewWidget.tsx
+++ src/components/home/TradingViewWidget.tsx
@@ -6,28 +6,36 @@ interface TradingViewWidgetProps {
 
 function TradingViewWidget({ symbol }: TradingViewWidgetProps) {
   const isCreated = useRef<boolean>(false);
-  const container = useRef<HTMLDivElement>();
+  const container = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (isCreated.current) return;
+    const containerElement = container.current;
 
-    const script = document.createElement("script");
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-    script.type = "text/javascript";
-    script.async = true;
-    script.innerHTML = JSON.stringify({
-      autosize: true,
-      symbol: symbol,
-      interval: "D",
-      timezone: "Asia/Seoul",
-      theme: "dark",
-      style: "1",
-      locale: "kr",
-      support_host: "https://www.tradingview.com",
-    });
-    container.current?.appendChild(script);
-    isCreated.current = true;
+    // containerElement가 null이 아닐 때만 작업 수행
+    if (containerElement) {
+      if (isCreated.current) {
+        // 기존 차트가 있다면 제거
+        containerElement.innerHTML = "";
+      }
+
+      const script = document.createElement("script");
+      script.src =
+        "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = JSON.stringify({
+        autosize: true,
+        symbol: symbol,
+        interval: "D",
+        timezone: "Asia/Seoul",
+        theme: "dark",
+        style: "1",
+        locale: "kr",
+        support_host: "https://www.tradingview.com",
+      });
+      container.current?.appendChild(script);
+      isCreated.current = true;
+    }
   }, [symbol]);
 
   return (
