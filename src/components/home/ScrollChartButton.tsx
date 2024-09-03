916eb520f09c0bcab32697162ddaa513e52e040b
Chaemin-L <chaem12@kookmin.ac.kr>
2024-09-04 07:12:07 +0900

    fix: 코인 변경에 따른 차트 리렌더링 불가 문제 해결

--- src/components/home/ScrollChartButton.tsx
+++ src/components/home/ScrollChartButton.tsx
@@ -44,48 +44,52 @@ const ScrollChartButton: React.FC<ScrollChartButtonProps> = ({ buttons }) => {
   };
 
   return (
-    <div className="-mx-[30px]">
-      <div>
-        <div
-          ref={scrollContainerRef}
-          className="scrollbar-hide overflow-x-auto whitespace-nowrap w-full cursor-pointer select-none"
-          onMouseDown={handleMouseDown}
-          onMouseLeave={handleMouseLeaveOrUp}
-          onMouseUp={handleMouseLeaveOrUp}
-          onMouseMove={handleMouseMove}
-        >
-          <div className="inline-flex space-x-[10px]">
-            {buttons.map((button, index) => (
-              <div key={index} className="flex-shrink-0">
+    <div className="-mx-[30px] relative ">
+      <div
+        ref={scrollContainerRef}
+        className=" scrollbar-hide overflow-x-auto whitespace-nowrap w-full cursor-pointer select-none"
+        onMouseDown={handleMouseDown}
+        onMouseLeave={handleMouseLeaveOrUp}
+        onMouseUp={handleMouseLeaveOrUp}
+        onMouseMove={handleMouseMove}
+      >
+        <div className="inline-flex space-x-[10px]">
+          {buttons.map((button, index) => (
+            <div key={index} className="flex-shrink-0">
+              <div
+                className={clsx(
+                  ` text-white p-4 rounded-[10px] min-w-[125px] min-h-[96px]`,
+                  selectedButtonIndex === index
+                    ? "bg-purple-400"
+                    : "bg-purple-600"
+                )}
+                onClick={() => handleButtonClick(index)}
+              >
+                <div className="text-sm font-medium">{button.label}</div>
+                <div className="text-lg font-bold">{button.value}</div>
                 <div
                   className={clsx(
-                    `text-white p-4 rounded-[10px] min-w-[125px] min-h-[96px]`,
-                    selectedButtonIndex === index
-                      ? "bg-purple-400"
-                      : "bg-purple-600"
+                    `text-xs`,
+                    button.isPositive ? "text-red" : "text-blue"
                   )}
-                  onClick={() => handleButtonClick(index)}
                 >
-                  <div className="text-sm font-medium">{button.label}</div>
-                  <div className="text-lg font-bold">{button.value}</div>
-                  <div
-                    className={clsx(
-                      `text-xs`,
-                      button.isPositive ? "text-red" : "text-blue"
-                    )}
-                  >
-                    {button.change}
-                  </div>
+                  {button.change}
                 </div>
               </div>
-            ))}
-          </div>
+              {selectedButtonIndex === index && (
+                <>
+                  <div className="h-[328px] w-full left-0">
+                    <div className="absolute top-[96px] mt-4 left-0 w-full h-[300px] ">
+                      <TradingViewWidget
+                        symbol={buttons[selectedButtonIndex].symbol}
+                      />
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+          ))}
         </div>
-        {selectedButtonIndex !== null && (
-          <div className="mt-4 w-full h-[300px]">
-            <TradingViewWidget symbol={buttons[selectedButtonIndex].symbol} />
-          </div>
-        )}
       </div>
     </div>
   );
