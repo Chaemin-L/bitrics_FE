e9fbd384118e48a65f8b0b7233e11dce778809f3
Chaemin-L <chaem12@kookmin.ac.kr>
2024-09-04 09:31:18 +0900

    style: 불필요한 코드 제거

--- src/components/home/TradingViewWidget.tsx
+++ src/components/home/TradingViewWidget.tsx
@@ -39,16 +39,7 @@ function TradingViewWidget({ symbol }: TradingViewWidgetProps) {
 
   return (
     <div className="tradingview-widget-container h-[300px]" ref={container}>
-      <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-        <a
-          href="https://www.tradingview.com/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
-      </div>
+      <div className="tradingview-widget-container__widget" />
     </div>
   );
 }
